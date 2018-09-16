44b0cf1cd28e38a4e800cff9d97d1063b713e780
Shengkai <shengkaixu123@hotmail.com>
2018-09-16 15:47:50 -0400

    9/16 Lab2B, NEED TO FIX

---
+++ Lab2B_StaticsProgram/Lab2B_StaticsProgram/header.h
@@ -0,0 +1,34 @@
+/////////////////////////////////////////////////////////////////////////////////////
+//                                                                     
+// Filename: header.h
+// Date: Sep 14, 2018
+// Programmer: Shengkai Xu
+//
+// Description:
+//    This program using given .dat to display:
+//		num >= 175, num < 175
+//		even num >= 175, odd num > 175
+//		sum of even num, sum of odd num, sum of all num
+//		largest num > 175, smallest num > 175
+//		calculate average and show laregst and smallest num in 4
+// 
+/////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Class: header
+//                                                                   
+// Description:
+//    All preloaded std functions for main function
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+#include "pch.h" 
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <Windows.h>
+
+using namespace std;
